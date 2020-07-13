10e9905d0229f3fd81fd83ca7763ef2f18823b4e
Craven <cawcraven@gmail.com>
2020-07-13 17:10:06 -0500

    localized talent description

--- scripts/mods/ranaldDump/ranaldDump.lua
+++ scripts/mods/ranaldDump/ranaldDump.lua
@@ -9,10 +9,43 @@ local function tableContains(table, key)
   return table[key] ~= nil
 end 
 
+-- Apparently # accomplishes this
 local function tableSize(table)
   local count = 0
   for _ in pairs(table) do count = count + 1 end
   return count
+end
+
+-- Not needed using Fatshark function
+local function getTalentDescription(description, values) 
+  local fmt_localized = Localize(description)
+  
+  if not values then
+    return fmt_localized
+  end
+  
+  local VALUE_LIST = {}
+  local n = #values
+  for i = 1, n, 1 do
+		local value_data = values[i]
+		local value_type = value_data.value_type
+		local value_fmt = value_data.value_fmt
+		local value = value_data.value
+
+		if value_type == "percent" then
+			value = math.abs(100 * value)
+		elseif value_type == "baked_percent" then
+			value = math.abs(100 * (value - 1))
+		end
+
+		if value_fmt then
+			value = string.format(value_fmt, value)
+		end
+
+		VALUE_LIST[i] = value
+  end
+  
+  return string.format(fmt_localized, unpack(VALUE_LIST, 1, n))
 end
 
 
@@ -22,31 +55,46 @@ mod:command("talents", " Talent info", function()
   local current_talents = talent_interface:get_talents("es_mercenary")
   -- mod:dump(CareerSettings["wh_zealot"], "talents", 1)
   mod:dump(Talents["witch_hunter"][1], "talents", 3)
-  local test = string.format(Localize(Talents["witch_hunter"][1]["description"]), 3)
-  mod:echo(test)
+  -- local test = string.format(Localize(Talents["witch_hunter"][1]["description"]), 3)
+  -- mod:echo(test)
   for key, value in pairs(Talents) do 
     mod:echo(key)
     for _, talent in pairs(value) do 
-      if tableContains(talent, "description_values") then
-        mod:echo(tableSize(talent["description_values"]))
+      local nameStatus, nameResponse = pcall(Localize, talent["name"])
+      if nameStatus then
+        mod:echo(nameResponse)
+        -- mod:echo("%s", getTalentDescription(talent["description"], talent["description_values"]))
+        mod:echo("%s", UIUtils.format_localized_description(talent["description"], talent["description_values"]))
       end
+      
     end
-    mod:dump(Talents[key], "talents", 3)
+    return
   end
-
-
-
-  talents = {}
-  -- for key, values in pairs(Talents) do 
-  --   mod:echo(key)
-  --   for _, talent in ipairs(values) do
-  --     talents[Localize(talent.name)] = talent.description
-  --     -- mod:echo(Localize(talent.name))
-  --     -- mod:echo(Localize(talent.description))
-  --   end
-  -- end
-  -- mod:echo(cjson.encode(talents))
 end)
+
+-- <talent>
+-- [icon] = victor_witchhunter_power_level_unbalance (string)
+-- [buffer] = server (string)
+-- [description_values] = table
+--   [1] = table
+--     [value_type] = percent (string)
+--     [value] = 0.074999999999999997 (number)
+-- [buffs] = table
+--   [1] = power_level_unbalance (string)
+-- [row] = 3 (number)
+-- [coulumn] = 3 (number)
+-- [buff_data] = table
+-- [num_ranks] = 1 (number)
+-- [tree] = 3 (number)
+-- [requirements] = table
+-- [name] = victor_witchhunter_power_level_unbalance (string)
+-- [description] = power_level_unbalance_desc (string)
+-- </talent>
+-- [MOD][ranaldDump][ECHO] <victor_placeholder>
+-- <talent>
+-- [name] = victor_placeholder (string)
+-- [description] = victor_placeholder (string)
+-- </talent>
 
 
 
